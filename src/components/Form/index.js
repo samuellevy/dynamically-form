fa64437dcc44c4e7f32cc02b3f9babd71bff9f9f
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-23 15:22:56 -0300

    many users with dynamically form working good

---
+++ src/components/Form/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import "./styles.css";
+// import { Container } from './styles';
+
+export default class Form extends Component {
+  state = {
+    buyer: {
+      name: "a",
+      email: ""
+    },
+    users: ["samuel"]
+  };
+  componentDidMount() {
+    console.log(this.state.users);
+  }
+
+  addUser = e => {
+    this.setState({ users: [...this.state.users, ""] });
+  };
+
+  handleBuyerChange = e => {
+    this.setState({ buyer: { ...this.state.buyer, name: e.target.value } });
+  };
+
+  handleChange = (e, index) => {
+    const users = this.state.users;
+    users[index] = e.target.value;
+    this.setState({ users: users });
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <form>
+          <label>Seu nome</label>
+          <input value={this.state.buyer.name} onChange={this.handleBuyerChange} />
+
+          {this.state.users.map((user, index) => (
+            <div className={"block"} key={index}>
+              <label>Nome</label>
+              <input value={user} onChange={e => this.handleChange(e, index)} />
+            </div>
+          ))}
+
+          <button type={"button"} onClick={this.addUser}>
+            Add
+          </button>
+          <button type={"button"} onClick={this.submitForm}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
